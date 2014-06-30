5839e491e60b765331191ce76fa65e2aee0a8524
Robin <84084888@qq.com>
2014-06-30 18:09:45 +0800

    add custom script

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var a = function ($, window, document, undefined) {
+	$(function () {
+		var ADAM = window.ADAM;
+
+		var $window   = $(window),
+			$document = $(document),
+			$html     = $(document.documentElement),
+			$body     = $(document.body),
+			$head     = $('head'),
+			$surface  = $body,
+			$content  = $('.content', $surface);
+	})
+}($, window, document)
+
